ed7ba3336cbfc72ae0b8508c8150115e42be3991
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-05-09 17:04:06 -0300

    Comenzando con la clase Conexion

---
+++ TP6-Grupo18-Programacion/Clases/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TP6_Grupo18_Programacion.Clases
+{
+    public class Conexion
+    {
+        string cadenaConexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True";
+
+        // Metodo establecer conexion
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        // Adapter para cargar GridView
+        public SqlDataAdapter ObtenerAdaptador(string consultaSQL)
+        {
+            // Usando adapter la conexion se cierra sola
+            SqlDataAdapter adapter;
+            try
+            {
+                adapter = new SqlDataAdapter(consultaSQL, ObtenerConexion());
+                return adapter;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
