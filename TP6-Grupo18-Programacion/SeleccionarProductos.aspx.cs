d2677e810c457d44476c2c6bdb22affbfd82e461
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-05-11 17:33:12 -0300

    GridView SeleccionarProductos

--- TP6-Grupo18-Programacion/SeleccionarProductos.aspx.cs
+++ TP6-Grupo18-Programacion/SeleccionarProductos.aspx.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP6_Grupo18_Programacion.Clases;
 
 namespace TP6_Grupo18_Programacion
 {
     public partial class SeleccionarProductos : System.Web.UI.Page
     {
+        Conexion conexion = new Conexion();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                CargarGridView();
+            }
         }
+
+        public void CargarGridView()
+        {
+            string consultaProductos = "SELECT * FROM Productos";
+            SqlDataAdapter adapter = conexion.ObtenerAdaptador(consultaProductos);
+            DataTable tabla = new DataTable();
+            adapter.Fill(tabla);
+
+            gvProductos.DataSource = tabla;
+            gvProductos.DataBind();
+        }
+
     }
 }
