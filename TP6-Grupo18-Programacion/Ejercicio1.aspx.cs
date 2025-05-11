3b1225e8208b58b1f6255a17af54b4f183b9536b
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-05-11 16:58:49 -0300

    segundo aporte ej 1

--- TP6-Grupo18-Programacion/Ejercicio1.aspx.cs
+++ TP6-Grupo18-Programacion/Ejercicio1.aspx.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP6_Grupo18_Programacion.Clases;
 
 namespace TP6_Grupo18_Programacion
 {
     public partial class Ejercicio1 : System.Web.UI.Page
     {
+        private Conexion conexion = new Conexion();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarGridView();
+            }
+        }
 
+        public void CargarGridView()
+        {
+            string consultaProductos = "SELECT * FROM Productos";
+            SqlDataAdapter adapter = conexion.ObtenerAdaptador(consultaProductos);
+            DataTable tabla = new DataTable();
+            adapter.Fill(tabla);
+
+            gvProducto.DataSource = tabla;
+            gvProducto.DataBind();
         }
     }
 }
